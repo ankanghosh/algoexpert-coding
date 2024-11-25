564c67d81b8eb808454072d7977f54a8300eaa4b
ankanghosh <novelinception@gmail.com>
2024-11-25 22:52:47 +0530

    Add binary_tree.py and BinaryTree.java to branch_sums

---
+++ java/easy/branch_sums/BinaryTree.java
@@ -0,0 +1,14 @@
+package main.java.com.example.branchsums;
+
+// Class to represent nodes in the Binary Tree.
+public class BinaryTree {
+	int value;
+	BinaryTree left;
+	BinaryTree right;
+
+	BinaryTree(int value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
---
+++ python/easy/branch_sums/binary_tree.py
@@ -0,0 +1,6 @@
+# Class to represent nodes in the Binary Tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
